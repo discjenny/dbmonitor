6721c4046b213e29962643fa427f8ee36f0ea17d
jwagner-mettrix <jwagner@mettrix.com>
2025-06-12 12:01:24 -0400

    return to localhost

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ async fn main() {
         .layer(axum_mw::from_fn(mw::logger))
         .with_state(db_pool);
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    println!("server running on http://127.0.0.1:3000");
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3010").await.unwrap();
+    println!("server running on http://127.0.0.1:3010");
     axum::serve(listener, app).await.unwrap();
 }
